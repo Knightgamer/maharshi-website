2a87f6ad2592a6e7442c20b94d369a6c21324211
Shivam Patel <sbp1784@gmail.com>
2025-12-24 17:18:52 +0300

    added gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 } from "next/font/google";
+import Script from "next/script";
 import "../static/styles/main.scss";
 
 const sourceSans = Source_Sans_3({
@@ -20,7 +21,24 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={sourceSans.className}>{children}</body>
+      <body className={sourceSans.className}>
+        {/* Google tag (gtag.js) */}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-48NGPTZBC3" strategy="afterInteractive" />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-48NGPTZBC3');
+            `,
+          }}
+        />
+
+        {children}
+      </body>
     </html>
   );
 }
